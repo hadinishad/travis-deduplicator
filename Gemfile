a96fc62a7dfbed7f3232f4264aaeed0f6ea3f02f
hadinishad <hadinishad42@gmail.com>
2016-02-16 18:04:59 +0530

    Working deduplicator

--- Gemfile
+++ Gemfile
@@ -34,3 +34,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 # Figare is used for our environment variables
 gem 'figaro'
+
+gem 'travis'
---
+++ app/controllers/repos_controller.rb
@@ -0,0 +1,26 @@
+class ReposController < ApplicationController
+  def deduplicate
+    begin
+      client = Travis::Client.new('https://api.travis-ci.com')
+      client.access_token = ENV["access_token"]
+      repository = client.repo("adwyze/#{params[:repo]}")
+      ids = []
+      cancelled = []
+
+      repository.each_build do |build|
+        if build.state == 'started' || build.state == 'queued'
+          if ids.include? build.pull_request_number
+            cancelled << build.pull_request_number
+            build.cancel if build.cancelable?
+          else
+            ids << build.pull_request_number
+          end
+        end
+      end
+
+      render json: cancelled
+    rescue => error
+      render json: error
+    end
+  end
+end
